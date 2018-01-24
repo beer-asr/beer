d21aeb9179996f1447f110429148b7baf4e413a8
Benes Karel <ibenes@fit.vutbr.cz>
2018-01-24 07:52:54 +0100

    ConjugateExponentialModel(): merge fit_step_() into fit()

--- beer/models/model.py
+++ beer/models/model.py
@@ -17,29 +17,26 @@ class Model(metaclass=abc.ABCMeta):
 class ConjugateExponentialModel(metaclass=abc.ABCMeta):
     '''Abstract base class for Conjugate Exponential models.'''
 
-    def _fit_step(self, mini_batch):
-        mini_batch_size = np.prod(mini_batch.shape[:-1])
-        scale = float(self._fit_cache['data_size']) / mini_batch_size
-        exp_llhs, acc_stats = self.exp_llh(mini_batch, accumulate=True)
-        exp_llh = np.sum(exp_llhs)
-        kld = self.kl_div_posterior_prior()
-        lower_bound = (scale * exp_llh - kld)
-        self.natural_grad_update(acc_stats, scale, self._fit_cache['lrate'])
-        return lower_bound / self._fit_cache['data_size'], \
-            exp_llh / self._fit_cache['data_size'], \
-            kld / self._fit_cache['data_size']
-
     def fit(self, data, mini_batch_size=-1, max_epochs=1, seed=None,
             lrate=1., callback=None):
-        self._fit_cache = {
-            'lrate': lrate,
-            'data_size': np.prod(data.shape[:-1])
-        }
+        data_size= np.prod(data.shape[:-1])
 
         mb_size = mini_batch_size if mini_batch_size > 0 else len(data)
         for epoch in range(1, max_epochs + 1):
             for mini_batch in mini_batches(data, mb_size, seed):
-                lower_bound, llh, kld = self._fit_step(mini_batch)
+                mini_batch_size = np.prod(mini_batch.shape[:-1])
+                scale = float(data_size) / mini_batch_size
+
+                exp_llhs, acc_stats = self.exp_llh(mini_batch, accumulate=True)
+                exp_llh = np.sum(exp_llhs)
+                kld = self.kl_div_posterior_prior()
+                lower_bound = (scale * exp_llh - kld)
+                self.natural_grad_update(acc_stats, scale, lrate)
+
+                lower_bound = lower_bound / data_size
+                llh = exp_llh / data_size
+                kld = kld / data_size
+
 
                 if callback is not None:
                     callback(lower_bound, llh, kld)
