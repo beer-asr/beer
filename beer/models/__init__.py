eab1379914b3f0a24e93b80495254b579a5b4564
iondel <iondel@fit.vutbr.cz>
2018-03-16 11:54:36 -0400

    Created a separated module for the MLPModel.

--- beer/models/__init__.py
+++ beer/models/__init__.py
@@ -5,6 +5,7 @@ from .normal import NormalFullCovariance
 from .mixture import Mixture
 
 from .vae import VAE
-from .vae import MLPNormalDiag
-from .vae import MLPNormalIso
+
+from .mlpmodel import MLPNormalDiag
+from .mlpmodel import MLPNormalIso
 
