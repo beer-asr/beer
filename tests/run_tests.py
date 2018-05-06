a07815acf04bc1877961b030e6b5c9243836c7c7
iondel <iondel@fit.vutbr.cz>
2018-05-06 16:36:33 -0400

    Added possibility to run 'n' times a test case with different seed values.

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -17,18 +17,21 @@ import test_expfamilyprior
 
 def run():
     parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--init-seed', type=int, default=1, 
+        help='initial seeding value for the random number generator')
+    parser.add_argument('--nruns', type=int, default=1, 
+        help='number of runs (with different seed values) for a test case')
     parser.add_argument('--tensor-type', choices=['float', 'double'], 
         default='float', help='type of the tensor to use in the tests')
-    parser.add_argument('--seed', type=int, default=1, 
-        help='seeding value for the random number generator')
     args = parser.parse_args()
 
     test_modules = [test_expfamilyprior]
     for test_module in test_modules:
         for testcase in test_module.__all__:
             suite = unittest.TestSuite()
-            suite.addTest(BaseTest.get_testsuite(testcase, 
-                tensor_type=args.tensor_type, seed=args.seed))
+            for i in range(args.nruns):
+                suite.addTest(BaseTest.get_testsuite(testcase, 
+                    tensor_type=args.tensor_type, seed=args.init_seed + i))
             unittest.TextTestRunner(verbosity=2).run(suite)
 
 
