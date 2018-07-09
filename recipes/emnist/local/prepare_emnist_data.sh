cbe1e0dc807c92b7395e6490c7bcb9d8ed2f41eb
iondel <iondel@fit.vutbr.cz>
2018-07-09 16:40:35 -0400

    Fixed: no downloading of the data every run.

--- recipes/emnist/local/prepare_emnist_data.sh
+++ recipes/emnist/local/prepare_emnist_data.sh
@@ -6,25 +6,28 @@ url=http://www.itl.nist.gov/iaui/vip/cs_links/EMNIST/gzip.zip
 batch_size=1000
 outdir=data
 
-mkdir -p ${outdir}
+# Download the data
+if [ ! -f "${outdir}/gzip.zip" ]; then
+    mkdir -p ${outdir}
+    wget $url -P "${outdir}" || exit 1
+else
+    echo "Data already downloaded. Skipping."
+fi
 
-
+# Extract and prepare the data.
 if [ ! -f "${outdir}/.done" ]; then
     mkdir -p "${outdir}"/rawdata
     mkdir -p "${outdir}"/letters/train "${outdir}"/letters/test
     mkdir -p "${outdir}"/digits/train "${outdir}"/digits/test
 
-    # Download the data
-    wget $url || exit 1
-
     # Extract the letters data set.
-    unzip -p gzip.zip gzip/emnist-letters-train-images-idx3-ubyte.gz | \
+    unzip -p ${outdir}/gzip.zip gzip/emnist-letters-train-images-idx3-ubyte.gz | \
         gunzip > "${outdir}/rawdata/emnist-letters-train-images.raw" || exit 1
-    unzip -p gzip.zip gzip/emnist-letters-train-labels-idx1-ubyte.gz | \
+    unzip -p ${outdir}/gzip.zip gzip/emnist-letters-train-labels-idx1-ubyte.gz | \
         gunzip > "${outdir}/rawdata/emnist-letters-train-labels.raw" || exit 1
-    unzip -p gzip.zip gzip/emnist-letters-test-images-idx3-ubyte.gz | \
+    unzip -p ${outdir}/gzip.zip gzip/emnist-letters-test-images-idx3-ubyte.gz | \
         gunzip > "${outdir}/rawdata/emnist-letters-test-images.raw" || exit 1
-    unzip -p gzip.zip gzip/emnist-letters-test-labels-idx1-ubyte.gz | \
+    unzip -p ${outdir}/gzip.zip gzip/emnist-letters-test-labels-idx1-ubyte.gz | \
         gunzip > "${outdir}/rawdata/emnist-letters-test-labels.raw" || exit 1
 
     python local/raw2npz.py \
@@ -44,13 +47,13 @@ if [ ! -f "${outdir}/.done" ]; then
         -name 'batch*npz' | sort -V > "${outdir}/letters/test/archives" || exit 1
 
     # Extract the digits data set.
-    unzip -p gzip.zip gzip/emnist-digits-train-images-idx3-ubyte.gz | \
+    unzip -p ${outdir}/gzip.zip gzip/emnist-digits-train-images-idx3-ubyte.gz | \
         gunzip > "${outdir}/rawdata/emnist-digits-train-images.raw" || exit 1
-    unzip -p gzip.zip gzip/emnist-digits-train-labels-idx1-ubyte.gz | \
+    unzip -p ${outdir}/gzip.zip gzip/emnist-digits-train-labels-idx1-ubyte.gz | \
         gunzip > "${outdir}/rawdata/emnist-digits-train-labels.raw" || exit 1
-    unzip -p gzip.zip gzip/emnist-digits-test-images-idx3-ubyte.gz | \
+    unzip -p ${outdir}/gzip.zip gzip/emnist-digits-test-images-idx3-ubyte.gz | \
         gunzip > "${outdir}/rawdata/emnist-digits-test-images.raw" || exit 1
-    unzip -p gzip.zip gzip/emnist-digits-test-labels-idx1-ubyte.gz | \
+    unzip -p ${outdir}/gzip.zip gzip/emnist-digits-test-labels-idx1-ubyte.gz | \
         gunzip > "${outdir}/rawdata/emnist-digits-test-labels.raw" || exit 1
 
     python local/raw2npz.py \
@@ -69,9 +72,9 @@ if [ ! -f "${outdir}/.done" ]; then
     find "${outdir}/digits/test" \
         -name 'batch*npz' | sort -V > "${outdir}/digits/test/archives" || exit 1
 
-    rm -fr gzip.zip "${outdir}/rawdata" || exit 1
+    rm -fr "${outdir}/rawdata" || exit 1
 
     date > "${outdir}/.done"
 else
-    echo "Data already downloaded. Skipping."
+    echo "Data already prepared. Skipping."
 fi
