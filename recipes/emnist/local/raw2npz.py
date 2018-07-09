99f762aba8e1c907d1698d18007f6c7606d5f0da
iondel <iondel@fit.vutbr.cz>
2018-07-09 15:54:24 -0400

    Data preparation for the EMNIST data.

---
+++ recipes/emnist/local/raw2npz.py
@@ -0,0 +1,49 @@
+'''Convert the raw MNIST format to a list of npz archives.'''
+
+import argparse
+import os
+import struct
+
+import numpy as np
+
+def run():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--batch-size', type=int, default=-1,
+                        help='batch size to split the data.')
+    parser.add_argument('images', help='path to the raw images')
+    parser.add_argument('labels', help='path to the raw labels')
+    parser.add_argument('outdir', help='output directory')
+    args = parser.parse_args()
+
+    # Read the labels.
+    with open(args.labels, 'rb') as fid:
+        _, num = struct.unpack(">II", fid.read(8))
+        labels = np.fromfile(fid, dtype=np.int8)
+
+    # Read the images.
+    with open(args.images, 'rb') as fid:
+        magic, num, rows, cols = struct.unpack(">IIII", fid.read(16))
+        images = np.fromfile(fid, dtype=np.uint8).reshape(num, rows, cols)
+
+    # For safety.
+    assert len(labels) == len(images)
+
+    if args.batch_size <= 0:
+        bsize = len(labels)
+    else:
+        bsize = args.batch_size
+
+    idx = 0
+    batchno = 1
+    while idx < len(images):
+        batch_images = images[idx: idx + bsize]
+        batch_images = batch_images.reshape(len(batch_images), -1)
+        batch_labels = labels[idx: idx + bsize]
+        outpath = os.path.join(args.outdir, 'batch' + str(batchno))
+        np.savez(outpath, features=batch_images, labels=batch_labels)
+        idx += bsize
+        batchno += 1
+
+
+if __name__ == '__main__':
+    run()
