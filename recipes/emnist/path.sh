4c15b74bc4bb6589d9c58ca6a56f9e617f157069
iondel <iondel@fit.vutbr.cz>
2018-07-10 10:58:17 -0400

    Activation of the beer environment.

--- recipes/emnist/path.sh
+++ recipes/emnist/path.sh
@@ -1,6 +1,6 @@
 
-# Set the specific environment variable to run the recipe. For instance
-# if "beer" is install in a specific python virtual environment called
-# "beer_env" and managed with anaconda:
-#    $ source activate beer_env
+# Set the specific environment variable to run the recipe.
+# By default, we assume that beer was installed on a specific
+# python virtual enviroment named "beer".
+source activate beer
 
