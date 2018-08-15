d04b0671cd63a2acfbc07af333a8463b71207fd4
Jinyi Yang <jyang126@jhu.edu>
2018-08-15 18:36:34 -0400

    Fix bug in generate phones.txt

--- recipes/timit_v2/local/timit_lang_prep.py
+++ recipes/timit_v2/local/timit_lang_prep.py
@@ -19,16 +19,16 @@ def main():
     with open(args.phonelist, 'r') as f:
         for line in f:
             tokens = line.strip().split()
-            if tokens[0] != 'q':
+            if tokens[0] != 'q' and tokens[1] != 'sil':
                 p_48 = tokens[1]
                 p_39 = tokens[2]
-            dict_phone_48[p_48] = 0
-            dict_map[p_48] = p_39
+                dict_phone_48[p_48] = 0
+                dict_map[p_48] = p_39
 
     with open(phone_48, 'w') as f1, open(phone_map, 'w') as f2:
         f1.write('sil 0\n')
         f2.write('sil sil\n')
-        for i, k in enumerate(sorted(dict_map.keys())):
+        for i, k in enumerate(sorted(dict_phone_48.keys())):
             if k != 'sil':
                 f1.write(k + ' ' + str(i+1) + '\n')
                 f2.write(k + ' ' + dict_map[k] + '\n')
