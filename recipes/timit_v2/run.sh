9593c889f7f416c76ce93b44b20b9b4e0ef30f10
Jinyi Yang <jyang126@jhu.edu>
2018-08-15 13:42:33 -0400

    Adding scripts to prepare phone file and phone.int.npz

--- recipes/timit_v2/run.sh
+++ recipes/timit_v2/run.sh
@@ -4,8 +4,24 @@
 # Load the configuration.
 . ./setup.sh
 
+stage=1
 
-echo =========================================================================
-echo "                         Data Preparation                              "
-echo =========================================================================
-local/timit_data_prep.sh "$timit"  || exit 1
+if [ $stage -le 0 ]; then
+    echo =========================================================================
+    echo "                         Data Preparation                              "
+    echo =========================================================================
+    local/timit_data_prep.sh "$timit"  || exit 1
+    python local/timit_lang_prep.py $langdir $confdir/phones.60-48-39.map
+fi
+
+if [ $stage -le 1 ]; then
+    for s in train test dev; do
+        mkdir -p $datadir/$s
+        cp $datadir/local/data/${s}_wav.scp $datadir/$s/wav.scp
+        cp $datadir/local/data/$s.uttids $datadir/$s/uttids
+        cp $datadir/local/data/$s.text $datadir/$s/text
+        # Feature extraction
+        python utils/prepare_trans.py \
+        $datadir/$s/text $langdir/phones_48.txt $datadir/$s
+    done
+fi
--- recipes/timit_v2/setup.sh
+++ recipes/timit_v2/setup.sh
@@ -1,7 +1,8 @@
 
 
 # Directory structure.
-export confdif=conf
+export confdir=conf
 export datadir=data
+export langdir=data/lang
 export timit=/export/corpora/LDC/LDC93S1/timit/TIMIT  # @JHU
 #timit=/mnt/matylda2/data/TIMIT/timit  # @BUT
