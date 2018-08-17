50bb2753fc4b769e68f6ec9bb22e5fcfb6157a8d
Jinyi Yang <jyang126@jhu.edu>
2018-08-17 16:58:47 -0400

    Add decoding scripts in last commit and modify setup.sh

--- recipes/timit_v2/setup.sh
+++ recipes/timit_v2/setup.sh
@@ -6,7 +6,7 @@ confdir=$(pwd)/conf
 datadir=$(pwd)/data
 data_train_dir=$(pwd)/data/train
 langdir=$(pwd)/data/lang
-root=$(pwd)/exp
+expdir=$(pwd)/exp
 
 
 # Features extraction.
@@ -31,11 +31,11 @@ vae_hmm_lrate_nnet=1e-3
 vae_hmm_batch_size=400
 vae_hmm_epochs=10
 vae_hmm_opts="--fast-eval --use-gpu"
-vae_hmm_dir=$root/vae_hmm_nw${vae_hmm_encoder_out_dim}_ldim${vae_hmm_latent_dim}
+vae_hmm_dir=$expdir/vae_hmm_nw${vae_hmm_encoder_out_dim}_ldim${vae_hmm_latent_dim}
 
 
 # HMM-GMM model parameters.
-hmm_gmm_mdl_dir=exp/hmm_gmm
+hmm_gmm_mdl_dir=$expdir/hmm_gmm
 hmm_conf=$confdir/hmm_gmm.yml
 hmm_infer_type='viterbi'
 hmm_lrate=0.1
@@ -43,3 +43,9 @@ hmm_batch_size=400
 hmm_epochs=10
 hmm_fast_eval="--fast-eval"
 use_gpu=""
+
+
+# Decode parameters.
+data_test_dir=$datadir/test
+decode_dir=$expdir/hmm_gmm/decode_test
+hmm_gamma=0.5 # HMM transition probability between phones
