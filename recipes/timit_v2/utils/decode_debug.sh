5ba1d042cf4474946fd164e0c2d7ad2afa82fe74
Jinyi Yang <jyang126@jhu.edu>
2018-08-21 13:42:31 -0400

    Add scripts to decode with multiple models

---
+++ recipes/timit_v2/utils/decode_debug.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $# -ne 3 ];then
+    echo "$0: <setup> <data-test-dir> <decode-modle-dir>"
+    exit 1
+fi
+
+setup=$1
+data_test_dir=$2
+mdl_dir=$3
+decode_dir=$mdl_dir/decode_test
+[ -f $mdl_dir/decode_all_result.txt ] && rm $mdl_dir/decode_all_result.txt
+touch $mdl_dir/decode_all_result.txt
+
+for n in $(seq 1 49); do
+    echo "Decoding with model $n"
+    mdl=$mdl_dir/$n.mdl
+    dir=${decode_dir}_${n}
+    #steps/decode_vae_hmm.sh $setup $data_test_dir $mdl $dir
+    cut -d " " -f 5 $dir/decode_result.txt >> $mdl_dir/decode_all_result.txt
+done
