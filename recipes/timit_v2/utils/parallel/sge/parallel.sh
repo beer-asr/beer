c31572a011d0b1d7384366c1c3a4713feebd44b5
Ondel Lucas <iondel@fit.vutbr.cz>
2018-09-10 17:29:41 +0200

    Added script to prepare batches for clustering.

--- recipes/timit_v2/utils/parallel/sge/parallel.sh
+++ recipes/timit_v2/utils/parallel/sge/parallel.sh
@@ -8,13 +8,15 @@ if [ $# -ne 5 ]; then
     exit 1
 fi
 
+
+
 name=$1
 opts=$2
 split_dir=$3
 cmd=$4
 log_dir=$5
 
-cmd=$(echo $cmd | sed s/%JOBID/$TASK_ID/g)
+
 rm -fr $log_dir/${name}.out.*
 qsub -N "$name"  -cwd  -j y -sync y\
     -o $log_dir/${name}.out'.$TASK_ID' \
