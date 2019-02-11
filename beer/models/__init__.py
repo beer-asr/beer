a9a67cc367f01090f047a054b2f126defdb3368a
iondel <iondel@fit.vutbr.cz>
2019-02-11 15:48:06 +0100

    Clean up noteboooks.

--- beer/models/__init__.py
+++ beer/models/__init__.py
@@ -5,16 +5,15 @@ from .basemodel import *
 #from .bayesmodel import *
 #from .modelset import *
 #from .parameters import *
-#
+
 ## Concrete model.
 from .normal import *
 from .mixture import *
 from .hmm import *
 #from .phoneloop import *
 #from .vae import *
-#
+
 ## Concrete model set.
-#from .mixtureset import *
-#from .marginalpldaset import *
+from .mixtureset import *
 from .normalset import *
 
